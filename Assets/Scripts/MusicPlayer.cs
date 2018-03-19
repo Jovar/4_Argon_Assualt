0ea9f6a4893d21fa020f7252f286266af9565280
JOHNWIN10\John <john.clement@gmail.com>
2018-03-18 23:54:21 -0700

    Reorganized Project

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,19 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        Invoke("LoadFirstScene", 2f);
-	}
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
-    }
 
     private void Awake()
     {
